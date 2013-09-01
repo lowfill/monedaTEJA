9e0b22ec552fb8b4e55c454854731bedc70981ae
Diego Andrés Ramírez Aragón <lowfill@gmail.com>
2013-09-01 18:25:28 +0200

    Added i18n code to templates

--- web/static/js/javascript.js
+++ web/static/js/javascript.js
@@ -1,11 +1,26 @@
+function loadTicker(div, url) {
+	$.get(url, function(data) {
+		$(div).html(data);
+	});
+	timer = setTimeout(function() {
+		loadTicker(url);
+	}, 3000);
+}
 
+function search() {
+	term = $('input#searchbox').val();
+	if (term.charAt(0) == '@') {
+		term = term.substring(1);
+	}
+	url = '/search/' + escape(term);
+	$.get(url, function(data) {
+		window.location = data;
+	});
+}
 
-        <script type="text/javascript">
-        
-        function Hello(){
-        
-            alert("hello");
-        
-        }
-        
-        </script>
+$(function() {
+	$('.disconnect form a').on('click', function(e) {
+		e.preventDefault();
+		$(this).parent().parent().submit();
+	});
+});
